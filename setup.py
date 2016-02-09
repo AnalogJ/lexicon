f3e35f146215d75f2caf5fc46fe4e3a15c714709
Jason Kulatunga <jk17@ualberta.ca>
2016-02-09 12:37:09 -0800

    bump version.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.7',
+    version='1.0.8',
 
     description='Manipulate DNS records on various DNS providers in a standardized/agnostic way',
     long_description=long_description,
