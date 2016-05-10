445fead51d4a9a2bfe1943074fa90b59acbde329
Jason Kulatunga <jk17@ualberta.ca>
2016-05-10 12:36:47 -0700

    added dnspod provider, added the ability to filter post data paramters

---
+++ tests/providers/test_dnspod.py
@@ -0,0 +1,16 @@
+# Test for one implementation of the interface
+from lexicon.providers.dnspod import Provider
+from integration_tests import IntegrationTests
+from unittest import TestCase
+import pytest
+
+# Hook into testing framework by inheriting unittest.TestCase and reuse
+# the tests which *each and every* implementation of the interface must
+# pass, by inheritance from define_tests.TheTests
+class DnsParkProviderTests(TestCase, IntegrationTests):
+
+    Provider = Provider
+    provider_name = 'dnspod'
+    domain = 'capsulecd.com'
+    def _filter_post_data_parameters(self):
+        return ['login_token']
