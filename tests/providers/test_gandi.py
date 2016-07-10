38269447300725a5173ae38b0386d2de574ca641
Philip Molter <github@hrunting.org>
2016-07-10 17:38:16 -0500

    Update Gandi test provider

--- tests/providers/test_gandi.py
+++ tests/providers/test_gandi.py
@@ -1,12 +1,12 @@
 # Test for one implementation of the interface
-from lexicon.providers.cloudflare import Provider
+from lexicon.providers.gandi import Provider
 from integration_tests import IntegrationTests
 from unittest import TestCase
 
 # Hook into testing framework by inheriting unittest.TestCase and reuse
 # the tests which *each and every* implementation of the interface must
 # pass, by inheritance from define_tests.TheTests
-class CloudflareProviderTests(TestCase, IntegrationTests):
+class GandiProviderTests(TestCase, IntegrationTests):
 
     Provider = Provider
     provider_name = 'gandi'
