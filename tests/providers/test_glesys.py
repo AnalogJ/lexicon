0b35016c02c46248adc45bc8dbaf41c4fa9d86e1
hecd <hecd744@gmail.com>
2017-03-28 16:17:02 +0200

    Added support for provider Glesys.

---
+++ tests/providers/test_glesys.py
@@ -0,0 +1,25 @@
+# Test for one implementation of the interface
+from lexicon.providers.glesys import Provider
+from integration_tests import IntegrationTests
+from unittest import TestCase
+import pytest
+
+# Hook into testing framework by inheriting unittest.TestCase and reuse
+# the tests which *each and every* implementation of the interface must
+# pass, by inheritance from define_tests.TheTests
+class GlesysProviderTests(TestCase, IntegrationTests):
+
+    Provider = Provider
+    provider_name = 'glesys'
+    domain = "capsulecd.com"
+
+    def _filter_headers(self):
+        return ['Authorization']
+
+    @pytest.mark.skip(reason="basic auth is used instead")
+    def test_Provider_authenticate(self):
+        return
+
+    @pytest.mark.skip(reason="basic auth is used instead")
+    def test_Provider_authenticate_with_unmanaged_domain_should_fail(self):
+        return
