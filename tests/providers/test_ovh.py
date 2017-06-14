99b1c49154d0a9fcd677c7b903f348e5f84d9db1
Adrien Ferrand <aferrand@ecomundo.eu>
2017-06-14 14:00:38 +0200

    Corrected integration tests

--- tests/providers/test_ovh.py
+++ tests/providers/test_ovh.py
@@ -12,40 +12,12 @@ class OvhProviderTests(TestCase, IntegrationTests):
     Provider = Provider
     provider_name = 'ovh'
     domain = 'elogium.net'
-    zoneFile = None
-
-    def _filter_post_data_parameters(self):
-        return ['login_token']
 
     def _filter_headers(self):
-        return ['Authorization']
-
-    def _filter_query_parameters(self):
-        return ['application_key', 'application_secret', 'consumer_key']
+        return ['X-Ovh-Application', 'X-Ovh-Consumer', 'X-Ovh-Signature']
 
     # Override _test_options to call env_auth_options and then import auth config from env variables
     def _test_options(self):
         cmd_options = env_auth_options(self.provider_name)
         cmd_options['domain'] = self.domain
         return cmd_options
-
-    # Make a backup of the targeted zone before launching tests
-    @classmethod
-    def setUpClass(cls):
-        cmd_options = env_auth_options(cls.provider_name)
-        cmd_options['domain'] = cls.domain
-        provider = Provider(cmd_options)
-        cls.zoneFile = provider.ovh_client.get('/domain/zone/{0}/export'.format(cls.domain))
-        print(cls.zoneFile)
-
-    # Restore the targeted zone after all tests done
-    @classmethod
-    def tearDownClass(cls):
-        print(cls.zoneFile)
-        cmd_options = env_auth_options(cls.provider_name)
-        cmd_options['domain'] = cls.domain
-        provider = Provider(cmd_options)
-        options = {
-            'zoneFile': cls.zoneFile
-        }
-        provider.ovh_client.post('/domain/zone/{0}/import'.format(cls.domain), **options)
