e3e3571f26213e2250780e3aaff8dbd1bd9913f1
Trinopoty Biswas <trinopoty@hotmail.com>
2018-03-08 12:47:21 +0530

    Fixed #182 for aurora and clouddns

--- lexicon/providers/aurora.py
+++ lexicon/providers/aurora.py
@@ -103,13 +103,20 @@ class Provider(BaseProvider):
     # If record does not exist, do nothing.
     def delete_record(self, identifier=None, type=None, name=None, content=None):
         # Try to find record if no identifier was specified
+        delete_record_id = []
         if not identifier:
-            identifier = self._find_record_identifier(type, name, None)
+            records = self.list_records(type, name, content)
+            delete_record_id = [record['id'] for record in records]
+        else:
+            delete_record_id.append(identifier)
+        
+        logger.debug('delete_records: %s', delete_record_id)
+        
+        for record_id in delete_record_id:
+            payload = self._delete('/zones/{0}/records/{1}'.format(self.domain_id, record_id))
 
-        payload = self._delete('/zones/{0}/records/{1}'.format(self.domain_id, identifier))
-
-        logger.debug('delete_record: %s', payload)
-        return payload
+        logger.debug('delete_record: %s', True)
+        return True
 
 
     # Helpers
