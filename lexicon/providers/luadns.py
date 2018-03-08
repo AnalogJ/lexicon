39133c788f30c08184806dfd5724e204b17536f9
Trinopoty Biswas <trinopoty@hotmail.com>
2018-03-08 12:14:16 +0530

    Fixed #182 for the mentioned providers

--- lexicon/providers/luadns.py
+++ lexicon/providers/luadns.py
@@ -90,14 +90,17 @@ class Provider(BaseProvider):
     # Delete an existing record.
     # If record does not exist, do nothing.
     def delete_record(self, identifier=None, type=None, name=None, content=None):
+        delete_record_id = []
         if not identifier:
             records = self.list_records(type, name, content)
-            logger.debug('records: %s', records)
-            if len(records) == 1:
-                identifier = records[0]['id']
-            else:
-                raise Exception('Record identifier could not be found.')
-        payload = self._delete('/zones/{0}/records/{1}'.format(self.domain_id, identifier))
+            delete_record_id = [record['id'] for record in records]
+        else:
+            delete_record_id.append(identifier)
+        
+        logger.debug('delete_records: %s', delete_record_id)
+        
+        for record_id in delete_record_id:
+            payload = self._delete('/zones/{0}/records/{1}'.format(self.domain_id, record_id))
 
         logger.debug('delete_record: %s', True)
         return True
