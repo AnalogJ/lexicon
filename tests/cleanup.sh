3dc74f5af85d9d0092cddda2b552e4815fa357e1
Jason Kulatunga <jk17@ualberta.ca>
2018-03-20 10:33:38 -0700

    added cleanup script.

---
+++ tests/cleanup.sh
@@ -0,0 +1,17 @@
+PROVIDER_NAME=pointhq
+lexicon $PROVIDER_NAME delete capsulecd.com TXT --name _acme-challenge.createrecordset.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com TXT --name _acme-challenge.deleterecordinset.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com TXT --name _acme-challenge.deleterecordset.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com TXT --name _acme-challenge.fqdn.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com TXT --name _acme-challenge.full.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com TXT --name _acme-challenge.listrecordset.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com TXT --name _acme-challenge.noop.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com TXT --name _acme-challenge.test.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com TXT --name random.fqdntest.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com TXT --name random.fulltest.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com TXT --name random.test.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com TXT --name updated.test.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com TXT --name updated.testfqdn.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com TXT --name updated.testfull.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com A --name localhost.capsulecd.com
+lexicon $PROVIDER_NAME delete capsulecd.com CNAME --name docs.example.com
