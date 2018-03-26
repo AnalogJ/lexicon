592574da3aefa44569d792cfc565897029579c08
Jason Kulatunga <jk17@ualberta.ca>
2018-03-25 17:58:20 -0700

    fixed extra tab in rackspace skip.

--- tests/providers/test_rackspace.py
+++ tests/providers/test_rackspace.py
@@ -36,24 +36,24 @@ class RackspaceProviderTests(TestCase, IntegrationTests):
         options['auth_token'] = None
         return options
 
-        @pytest.mark.skip(reason="TODO: extended test suite, needs contributor to implement")
-        def test_Provider_when_calling_list_records_with_invalid_filter_should_be_empty_list(self):
-            return
-        @pytest.mark.skip(reason="TODO: extended test suite, needs contributor to implement")
-        def test_Provider_when_calling_list_records_should_handle_record_sets(self):
-            return
+    @pytest.mark.skip(reason="TODO: extended test suite, needs contributor to implement")
+    def test_Provider_when_calling_list_records_with_invalid_filter_should_be_empty_list(self):
+        return
+    @pytest.mark.skip(reason="TODO: extended test suite, needs contributor to implement")
+    def test_Provider_when_calling_list_records_should_handle_record_sets(self):
+        return
 
-        @pytest.mark.skip(reason="TODO: extended test suite, needs contributor to implement")
-        def test_Provider_when_calling_delete_record_with_record_set_name_remove_all(self):
-            return
+    @pytest.mark.skip(reason="TODO: extended test suite, needs contributor to implement")
+    def test_Provider_when_calling_delete_record_with_record_set_name_remove_all(self):
+        return
 
-        @pytest.mark.skip(reason="TODO: extended test suite, needs contributor to implement")
-        def test_Provider_when_calling_delete_record_with_record_set_by_content_should_leave_others_untouched(self):
-            return
+    @pytest.mark.skip(reason="TODO: extended test suite, needs contributor to implement")
+    def test_Provider_when_calling_delete_record_with_record_set_by_content_should_leave_others_untouched(self):
+        return
 
-        @pytest.mark.skip(reason="TODO: extended test suite, needs contributor to implement")
-        def test_Provider_when_calling_create_record_with_duplicate_records_should_be_noop(self):
-            return
+    @pytest.mark.skip(reason="TODO: extended test suite, needs contributor to implement")
+    def test_Provider_when_calling_create_record_with_duplicate_records_should_be_noop(self):
+        return
 
         @pytest.mark.skip(reason="TODO: extended test suite, needs contributor to implement")
         def test_Provider_when_calling_create_record_multiple_times_should_create_record_set(self):
