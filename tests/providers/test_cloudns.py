0aa2437935634c61f50dbf1dc7a6dc4e2459cefb
Pascal Mathis <mail@pascalmathis.com>
2018-03-27 18:36:40 +0200

    cloudns: Fixes and recordings for PR #203
    Provide missing test cassetes for PR #203 which improved handling of
    duplicate records. The following changes have been made previously to
    recording the cassettes:
    
     - Removed workaround for overriding authentication environment
    variables, due to bug being fixed in commit 970dd9e
    
     - Added code to the cloudns provider for detecting if an option was
    given by the user or not. This is required due to the integration tests
    using `lambda x: 'placeholder_' + x` as their *fallbackFn*
    implementation, which will break the auth_id/subid/subuser options,
    where only exactly one option can be specified.
    
    As these changes might potentially affect other cassettes as well, all
    recordings have been regenerated with the updated test suite. Thanks to
    the adjustments done to the test suite, it is now also possible to
    record cassettes with a restricted sub-account API instead of granting
    full access.

--- tests/providers/test_cloudns.py
+++ tests/providers/test_cloudns.py
@@ -1,6 +1,5 @@
 # Test for one implementation of the interface
 from lexicon.providers.cloudns import Provider
-from lexicon.common.options_handler import env_auth_options
 from integration_tests import IntegrationTests
 from unittest import TestCase
 import pytest
@@ -19,9 +18,3 @@ class CloudnsProviderTests(TestCase, IntegrationTests):
 
     def _filter_post_data_parameters(self):
         return ['auth-id', 'sub-auth-id', 'sub-auth-user', 'auth-password']
-
-    # Override _test_options to call env_auth_options and then import auth config from env variables
-    def _test_options(self):
-        cmd_options = super(CloudnsProviderTests, self)._test_options()
-        cmd_options.update(env_auth_options(self.provider_name))
-        return cmd_options
