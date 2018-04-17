6b217e1cc8010e095bc01e17051114b9c6102314
Patrick Kane <pmk@wawd.com>
2018-04-17 10:49:56 -0700

    test harness for the constellix provider.

---
+++ tests/providers/test_constellix.py
@@ -0,0 +1,17 @@
+from lexicon.providers.constellix import Provider
+from integration_tests import IntegrationTests
+from unittest import TestCase
+import pytest
+
+# Constellix does not currently have a sandbox and they enforce domain
+# uniqueness across the service.  You'll need your own production credentials
+# and a unique domain name if you want to run these tests natively.
+class ConstellixProviderTests(TestCase, IntegrationTests):
+
+    Provider = Provider
+    provider_name = 'constellix'
+    domain = 'example.org'
+
+    def _filter_headers(self):
+        return ['x-cnsdns-apiKey', 'x-cnsdns-hmac']
+
