89320ca1e3573ab78345eb1ed4d49096ed43c770
Adrien Ferrand <aferrand@ecomundo.eu>
2018-06-13 12:26:18 +0200

    Add comments on gandi provider

--- lexicon/providers/gandi.py
+++ lexicon/providers/gandi.py
@@ -1,3 +1,30 @@
+"""Provide support to Lexicon for Gandi LiveDNS and Gandi XMLRPC changes.
+
+Lexicon provides a common interface for querying and managing DNS services
+through those services' APIs. This module implements the Lexicon interface
+against the Gandi API.
+
+Gandi introduced the LiveDNS API (http://doc.livedns.gandi.net/) in 2017.
+It is the successor of the traditional XMLRPC API, which suffered from 
+long delays between API-based changes and their activation.
+The LiveDNS API has one significant peculiarity: DNS records with the
+same name and type are managed as one unit. Thus records cannot be
+addressed distinctly, which complicates removal of records significantly.
+
+The Gandi XMLRPC API is different from typical DNS APIs in that Gandi
+zone changes are atomic. You cannot edit the currently active
+configuration. Any changes require editing either a new or inactive
+configuration. Once the changes are committed, then the domain is switched
+to using the new zone configuration. This module makes no attempt to
+cleanup previous zone configurations.
+
+Note that Gandi domains can share zone configurations. In other words,
+I can have domain-a.com and domain-b.com which share the same zone
+configuration file. If I make changes to domain-a.com, those changes
+will only apply to domain-a.com, as domain-b.com will continue using
+the previous version of the zone configuration. This module makes no
+attempt to detect and account for that.
+"""
 import json
 import logging
 import requests
@@ -15,10 +42,14 @@ LOGGER = logging.getLogger(__name__)
 def ProviderParser(subparser):
     """Specify arguments for Gandi Lexicon Provider."""
     subparser.add_argument('--auth-token', help="specify Gandi API key")
-    subparser.add_argument('--api-protocol', help="specify Gandi API protocol to use (rcp or rest, rpc by default if not specified)")
+    subparser.add_argument('--api-protocol', help="(optional) specify Gandi API protocol to use: rpc (default) or rest")
 
 class Provider(BaseProvider):
+    """Provide Gandi LiveDNS API implementation of Lexicon Provider interface.
 
+    Note that this implementation will delegates its call to GandiRPCSubProvider
+    if RPC protocol is used.
+    """
     def __init__(self, options, engine_overrides=None):
         super(Provider, self).__init__(options)
         self.default_ttl = 3600
@@ -65,7 +96,11 @@ class Provider(BaseProvider):
         LOGGER.debug('create_record: %s', True)
         return True
 
+    # List all records. Return an empty list if no records found
+    # type, name and content are used to filter records.
+    # If possible filter during the query, otherwise filter after response is received.
     def list_records(self, type=None, name=None, content=None):
+        """List all record for the domain in the active Gandi zone."""
         if (self.protocol == 'rpc'):
             return self.rpc_helper.list_records(type, name, content)
 
@@ -108,7 +143,12 @@ class Provider(BaseProvider):
         LOGGER.debug('list_records: %s', records)
         return records
 
+    # Update a record. Identifier must be specified.
     def update_record(self, identifier, type=None, name=None, content=None):
+        """Updates the specified record in a new Gandi zone
+
+        'content' should be a string or a list of strings
+        """
         if (self.protocol == 'rpc'):
             return self.rpc_helper.update_record(identifier, type, name, content)
 
@@ -136,6 +176,8 @@ class Provider(BaseProvider):
         LOGGER.debug('update_record: %s', True)
         return True
 
+    # Delete existings records.
+    # If records do not exist, do nothing.
     def delete_record(self, identifier=None, type=None, name=None, content=None):
         if (self.protocol == 'rpc'):
             return self.rpc_helper.delete_record(identifier, type, name, content)
@@ -173,8 +215,9 @@ class Provider(BaseProvider):
 
         # is always True at this point, if a non 200 response is returned an error is raised.
         LOGGER.debug('delete_record: %s', True)
-        return True           
+        return True
 
+    # Helpers
     def _request(self, action='GET',  url='/', data=None, query_params=None):
         if data is None:
             data = {}
@@ -198,15 +241,14 @@ class Provider(BaseProvider):
         else:
             return r.json()
 
+
 class GandiRPCSubProvider(object):
-
-    # This exception is for cleaner handling of internal errors
-    # within the Gandi provider codebase
-    class GandiInternalError(Exception):
-        """Internal exception handling class for Gandi management errors"""
-        pass
-
+    """Provide Gandi RPCXML API implementation of Lexicon Provider interface.
+    This implementation is called through the main LiveDNS implementation
+    is RPC protocol is used.
+    """
     def __init__(self, api_key, api_endpoint, domain, relative_name_fn, full_name_fn):
+        """Initialize Gandi RCPXML API provider."""
         super(GandiRPCSubProvider, self).__init__()
 
         self._api_endpoint = api_endpoint
@@ -216,7 +258,12 @@ class GandiRPCSubProvider(object):
         self._full_name = full_name_fn
         self._api = xmlrpclib.ServerProxy(self._api_endpoint, allow_none=True)
 
+    # Authenticate against provider,
+    # Make any requests required to get the domain's id for this provider,
+    # so it can be used in subsequent calls. Should throw an error if
+    # authentication fails for any reason, or if the domain does not exist.
     def authenticate(self):
+        """Determine the current domain and zone IDs for the domain."""
         try:
             payload = self._api.domain.info(self._api_key, self._domain)
             self._zone_id = payload['zone_id']
@@ -224,7 +271,9 @@ class GandiRPCSubProvider(object):
         except xmlrpclib.Fault as err:
             raise Exception("Failed to authenticate: '{0}'".format(err))
 
+    # Create record. If record already exists with the same content, do nothing.
     def create_record(self, type, name, content, ttl):
+        """Creates a record for the domain in a new Gandi zone."""
         version = None
         ret = False
 
@@ -249,7 +298,11 @@ class GandiRPCSubProvider(object):
         LOGGER.debug("create_record: %s", ret)
         return ret
 
+    # List all records. Return an empty list if no records found
+    # type, name and content are used to filter records.
+    # If possible filter during the query, otherwise filter after response is received.
     def list_records(self, type=None, name=None, content=None):
+        """List all record for the domain in the active Gandi zone."""
         opts = {}
         if type is not None:
             opts['type'] = type.upper()
@@ -278,7 +331,9 @@ class GandiRPCSubProvider(object):
         LOGGER.debug("list_records: %s", records)
         return records
 
-    def update_record(self, identifier, type=None, name=None, content=None):      
+    # Update a record. Identifier or type+name+content
+    def update_record(self, identifier, type=None, name=None, content=None): 
+        """Updates the specified record in a new Gandi zone."""     
         if not identifier:
             records = self.list_records(type, name)
             if len(records) == 1:
@@ -336,7 +391,11 @@ class GandiRPCSubProvider(object):
         LOGGER.debug("update_record: %s", ret)
         return ret
 
+    # Delete existing records.
+    # If records do not exist, do nothing.
+    # If an identifier is specified, use it, otherwise do a lookup using type, name and content.
     def delete_record(self, identifier=None, type=None, name=None, content=None):
+        """Removes the specified records in a new Gandi zone."""
         version = None
         ret = False
 
@@ -368,7 +427,7 @@ class GandiRPCSubProvider(object):
                     self._api.domain.zone.version.delete(self._api_key, self._zone_id, version)
 
         LOGGER.debug("delete_record: %s", ret)
-        return ret    
+        return ret  
 
     @staticmethod
     def _txt_encode(val):
@@ -383,4 +442,10 @@ class GandiRPCSubProvider(object):
         if len(val) > 1 and val[0:1] == '"':
             val = val[1:-1].replace('" "', '').replace('\\"', '"').replace('\\\\', '\\')
         return val
+
+    # This exception is for cleaner handling of internal errors
+    # within the Gandi provider codebase
+    class GandiInternalError(Exception):
+        """Internal exception handling class for Gandi management errors"""
+        pass
 
