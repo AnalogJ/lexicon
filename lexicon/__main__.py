ae9ec17e35f0dca775722881ad455ed78ec0fc28
Adrien Ferrand <aferrand@ecomundo.eu>
2018-07-12 13:27:37 +0200

    Get an output to the command line, with various parameters to control this output.

--- lexicon/__main__.py
+++ lexicon/__main__.py
@@ -7,6 +7,7 @@ import importlib
 import logging
 import os
 import sys
+import json
 
 import pkg_resources
 
@@ -28,7 +29,10 @@ def BaseProviderParser():
     parser.add_argument("--ttl", type=int, help="specify the record time-to-live")
     parser.add_argument("--priority", help="specify the record priority")
     parser.add_argument("--identifier", help="specify the record for update or delete actions")
-    parser.add_argument("--log_level", help="specify the log level", default="DEBUG", choices=["CRITICAL","ERROR","WARNING","INFO","DEBUG","NOTSET"])
+    parser.add_argument("--log_level", help="specify the log level", default="INFO", choices=["CRITICAL","ERROR","WARNING","INFO","DEBUG","NOTSET"])
+    parser.add_argument("--quiet", help="If set, do not print command result on stdout", action='store_true')
+    parser.add_argument("--json", help="If set, return print command on stdout as a json string", action='store_true')
+    parser.add_argument("--no-headers", help="If set, return print command without table headers", action='store_true')
     return parser
 
 def MainParser():
@@ -60,17 +64,64 @@ def MainParser():
 
     return parser
 
-#dynamically determine all the providers available.
-def main():
+# Print returned JSON into a nice table for command line usage
+def print_json_result(logger, output=None, without_header=None):
+    try:
+        _ = (entry for entry in output)
+    except:
+        logger.debug('Command output is not iterable, and then cannot be printed with --quiet parameter not enabled.')
+        return
 
+    array = [[row['id'], row['type'], row['name'], row['content'], row['ttl']] for row in output]
+
+    # Insert header (insert before calculating the max width of each column to take headers size into account)
+    if not without_header:
+        headers = ['ID', 'TYPE', 'NAME', 'CONTENT', 'TTL']
+        array.insert(0, headers)
+
+    columnWidths = [0, 0, 0, 0, 0]
+    # Find max width for each column
+    for row in array:
+        for idx, col in enumerate(row):
+            width = len(str(col))
+            if width > columnWidths[idx]:
+                columnWidths[idx] = width
+
+    # Add a "nice" separator
+    if not without_header:
+        array.insert(1, ['-' * columnWidths[idx] for idx in range(len(columnWidths))])
+
+    # Construct table to be printed
+    print_table = []
+    for row in array:
+        rowList = []
+        for idx, col in enumerate(row):
+            rowList.append(str(col).ljust(columnWidths[idx]))
+        print_table.append(' '.join(rowList))
+
+    # Print table
+    print('\n'.join(print_table))
+
+# Dynamically determine all the providers available.
+def main():
     parsed_args = MainParser().parse_args()
     log_level = logging.getLevelName(parsed_args.log_level)
     logging.basicConfig(stream=sys.stdout, level=log_level, format='%(message)s')
 
     logger.debug('Arguments: %s', parsed_args)
     client = Client(parsed_args.__dict__)
-    client.execute()
+    
+    output = client.execute()
 
+    if not parsed_args.__dict__['quiet']:
+        if not parsed_args.__dict__['json']:
+            print_json_result(logger, output, parsed_args.__dict__['no_headers'])
+        else:
+            try:
+                print(json.dumps(output))
+            except:
+                logger.debug('Output is not a JSON, and then cannot be printed with --json parameter enabled.')
+                pass
 
 if __name__ == '__main__':
     main()
