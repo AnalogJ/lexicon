e5153a73b50cf640a0fcd7664e217401357ff47a
Rezart Qelibari <it@astzweig.de>
2018-10-01 10:21:01 +0200

    [easyname] Init provider with arguments

--- lexicon/providers/easyname.py
+++ lexicon/providers/easyname.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
+from .base import Provider as BaseProvider
+
 def ProviderParser(subparser):
     subparser.description = """A provider for Easyname DNS."""
     subparser.add_argument(
@@ -11,3 +13,12 @@ def ProviderParser(subparser):
         '--auth-password',
         help='Specify password used to authenticate',
     )
+
+
+class Provider(BaseProvider):
+    """
+        easyname provider
+    """
+
+    def __init__(self, options, engine_overrides=None):
+        super(Provider, self).__init__(options, engine_overrides)
