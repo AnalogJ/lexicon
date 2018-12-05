2562e9805700ce3f26166711fe2eda98ec250ee7
Adrien Ferrand <adrien.ferrand@amadeus.com>
2018-12-05 11:14:36 +0100

    Fix lint

--- tests/pylint_quality_gate.py
+++ tests/pylint_quality_gate.py
@@ -5,42 +5,57 @@ import sys
 import os
 import shutil
 import tempfile
+import contextlib
 import stat
+from io import StringIO
 
 from pylint import lint
 
-REPO_DIR = os.path.dirname(os.path.dirname(__file__))
+
+REPO_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+@contextlib.contextmanager
+def capture():
+    oldout, olderr = sys.stdout, sys.stderr
+    try:
+        out = [StringIO(), StringIO()]
+        sys.stdout, sys.stderr = out
+        yield out
+    finally:
+        sys.stdout, sys.stderr = oldout, olderr
+        out[0] = out[0].getvalue()
+        out[1] = out[1].getvalue()
 
 
 def get_pylint_upstream_master_note():
     """
     Get the pylint global note of lexicon on upstream master branch
     """
-    sys.stdout.write('Preparing a temporary local repository for upstream ...\n')
+    sys.stdout.write(
+        '===> Preparing a temporary local repository for upstream ... <===\n')
     worktree_dir = tempfile.mkdtemp()
 
-    score = None
-
     try:
-        subprocess.call('git clone --depth 1 https://github.com/AnalogJ/lexicon.git {0}'
-                        .format(worktree_dir), shell=True)
+        sys.stdout.write('===> Executing pylint on upstream master '
+                         'to calculate pylint global note diff ... <===\n')
 
-        sys.stdout.write('Executing pylint on upstream master '
-                         'to calculate pylint global note diff ...\n')
-        command = ('{0} -c "import sys; from pylint.lint import Run; '
-                   'results = Run([\'lexicon\', \'tests\'], do_exit=False); '
-                   'sys.stdout.write(str(results.linter.stats[\'global_note\']));"'
-                   .format(sys.executable))
-        stdout = subprocess.check_output(command, shell=True, 
-                                         cwd=worktree_dir, universal_newlines=True)
-        score = float(stdout.strip().split('\n')[-1])
+        subprocess.check_output([
+            'git', 'clone', '--depth=1', 'https://github.com/AnalogJ/lexicon.git',
+            worktree_dir], stderr=subprocess.STDOUT)
+        subprocess.check_output(['pip', 'install', '-e', worktree_dir], stderr=subprocess.STDOUT)
+        with capture():
+            results = lint.Run([
+                os.path.join(worktree_dir, 'lexicon'), os.path.join(worktree_dir, 'tests'),
+                os.path.join(worktree_dir, 'tests', 'providers'), '--persistent=n'],
+                do_exit=False)
+
+        return results.linter.stats['global_note']
     finally:
         def del_rw(_, name, __):
             os.chmod(name, stat.S_IWRITE)
             os.remove(name)
         shutil.rmtree(worktree_dir, onerror=del_rw)
-
-    return score
 
 
 def quality_gate(stats, upstream_master_note):
@@ -49,6 +64,10 @@ def quality_gate(stats, upstream_master_note):
     Raise if these metrics do not match expectations.
     """
     quality_errors = []
+
+    sys.stdout.write('=====================\n')
+    sys.stdout.write('Quality gate results:\n')
+    sys.stdout.write('=====================\n')
 
     if stats['fatal']:
         quality_errors.append('Quality gate failure: {0} "fatal" issues have been found.\n'
@@ -73,17 +92,24 @@ def quality_gate(stats, upstream_master_note):
 
     return 0
 
+
 def main():
     """Main process"""
     upstream_master_note = get_pylint_upstream_master_note()
 
-    sys.stdout.write('Executing pylint on current branch ...\n')
+    # Script is located two levels deep in the repository root (./tests/pylint_quality_gate.py)
+    repo_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    sys.stdout.write('===> Executing pylint on current branch ... <===\n')
+    subprocess.check_output(['pip', 'install', '-e', repo_dir], stderr=subprocess.STDOUT)
     results = lint.Run([
-        os.path.join(REPO_DIR, 'lexicon'), os.path.join(REPO_DIR, 'tests'), '--persistent=n'],
-                       do_exit=False)
+        os.path.join(repo_dir, 'lexicon'), os.path.join(repo_dir, 'tests'),
+        os.path.join(repo_dir, 'tests', 'providers'), '--persistent=n'],
+        do_exit=False)
 
     stats = results.linter.stats
     sys.exit(quality_gate(stats, upstream_master_note))
+
 
 if __name__ == '__main__':
     main()
