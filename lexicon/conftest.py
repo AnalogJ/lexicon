f1e3e1427e08e963cf46fb369cf75223b92b5c28
Adrien Ferrand <ferrand.ad@gmail.com>
2019-02-05 00:39:30 +0100

    Xfail tests for which provider dependencies are not fulfilled

--- lexicon/conftest.py
+++ lexicon/conftest.py
@@ -1,27 +1,27 @@
 """General pytest configuration for lexicon tests."""
 import pytest
-import pkg_resources
 
-LEXICON_DISTRIBUTION = pkg_resources.require('dns-lexicon')[0]
-PROVIDERS_WITH_OPTIONALS = [provider for provider in LEXICON_DISTRIBUTION.extras
-                            if provider not in ('dev', 'full')]
+from lexicon import discovery
 
 
 def pytest_addoption(parser):
     """Standard pytest hook invoked to add options to pytest CLI"""
-    parser.addoption('--skip-providers-with-optdeps', action='store_true',
+    parser.addoption('--skip-providers-with-missing-deps', action='store_true',
                      help='Skip tests on providers with optional dependencies')
 
 
 def pytest_runtest_setup(item):
     """Standard pytest hook invoked before each test execution"""
     try:
-        skip_providers_with_optdeps = getattr(item.config.option, 'skip_providers_with_optdeps')
+        skip_providers_with_optdeps = getattr(item.config.option, 'skip_providers_with_missing_deps')
     except AttributeError:
         pass
     else:
-        skip = [provider for provider in PROVIDERS_WITH_OPTIONALS
-                if provider in item.parent.name.lower()]
-        if skip_providers_with_optdeps and skip:
-            pytest.skip('Test skipped because --skip-providers-with-optdeps '
-                        'is set and provider is marked as having optional dependencies.')
+        print(skip_providers_with_optdeps)
+        if skip_providers_with_optdeps:
+            providers = discovery.find_providers()
+            skip = [available for provider, available in providers.items()
+                    if provider in item.parent.name.lower()]
+            if skip and not skip[0]:
+                pytest.xfail('Test skipped because --skip-providers-with-missing-deps '
+                             'is set and provider has missing required dependencies.')
