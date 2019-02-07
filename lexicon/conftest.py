94fc31b1215a0bdc285365d55c9000bd31d8b843
Adrien Ferrand <ferrand.ad@gmail.com>
2019-02-07 11:52:20 +0100

    Rename skip => xfail

--- lexicon/conftest.py
+++ lexicon/conftest.py
@@ -6,14 +6,14 @@ from lexicon import discovery
 
 def pytest_addoption(parser):
     """Standard pytest hook invoked to add options to pytest CLI"""
-    parser.addoption('--skip-providers-with-missing-deps', action='store_true',
+    parser.addoption('--xfail-providers-with-missing-deps', action='store_true',
                      help='Skip tests on providers with optional dependencies')
 
 
 def pytest_runtest_setup(item):
     """Standard pytest hook invoked before each test execution"""
     try:
-        skip_providers_with_optdeps = getattr(item.config.option, 'skip_providers_with_missing_deps')
+        skip_providers_with_optdeps = getattr(item.config.option, 'xfail_providers_with_missing_deps')
     except AttributeError:
         pass
     else:
@@ -23,5 +23,5 @@ def pytest_runtest_setup(item):
             skip = [available for provider, available in providers.items()
                     if provider in item.parent.name.lower()]
             if skip and not skip[0]:
-                pytest.xfail('Test skipped because --skip-providers-with-missing-deps '
+                pytest.xfail('Test expected to fail because --skip-providers-with-missing-deps '
                              'is set and provider has missing required dependencies.')
