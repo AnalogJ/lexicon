0a9e2a94b4c0e59725605b6535f86d2ece01dab5
Claud.Lee <wildcloud3@gmail.com>
2019-05-22 02:55:15 +0800

    add support for Aliyun (#406)
    * add basic support for aliyun
    
    * pass test
    
    * fix file place
    
    * change test file path
    
    * refactor to api, enable recording by vcrpy
    
    * add test recordings
    
    * compatiblity for python3
    
    * update test results
    
    * remove recordings
    
    * fix lint and query_prarameter filters
    
    * remove wrong added env files
    
    * add back correct test records
    
    * fix last two lint bugs
    
    * resolve adferrand's comments
    
    * sorry for the missed one

---
+++ lexicon/tests/providers/test_aliyun.py
@@ -0,0 +1,20 @@
+"""test module for aliyun provider"""
+
+# Test for one implementation of the interface
+from unittest import TestCase
+from lexicon.tests.providers.integration_tests import IntegrationTests
+
+# Hook into testing framework by inheriting unittest.TestCase and reuse
+# the tests which *each and every* implementation of the interface must
+# pass, by inheritance from integration_tests.IntegrationTests
+class AliyunProviderTests(TestCase, IntegrationTests):
+    """Integration tests for Foo provider"""
+    provider_name = 'aliyun'
+    domain = 'mean.space'
+
+    def _filter_query_parameters(self):
+        """filter access key id && secret info"""
+        return [('AccessKeyId', 'DUMMY_KEY_ID'),
+                ('Signature', 'DUMMY_SIGNATURE'),
+                ('SignatureNonce', 'DUMMY_SIGNATURE_NONCE'),
+                ('Timestamp', 'DUMMY_TIMESTAMP')]
