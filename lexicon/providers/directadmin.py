9dae3caee6db55c133d517f657a6080e2ae0de66
Joris Kraak <me@joriskraak.nl>
2019-06-17 22:30:45 +0200

    Add `endpoint` argument to specify DirectAdmin endpoint

--- lexicon/providers/directadmin.py
+++ lexicon/providers/directadmin.py
@@ -3,12 +3,19 @@ from lexicon.providers.base import Provider as BaseProvider
 
 def provider_parser(subparser):
     """Return the parser for this provider"""
-    subparser
+    subparser.add_argument(
+        "--endpoint",
+        help = "specify the DirectAdmin endpoint"
+    )
 
 class Provider(BaseProvider):
     """Provider class for DirectAdmin"""
     def __init__(self, config):
         super(Provider, self).__init__(config)
+
+        self.endpoint = self._get_provider_option('endpoint')
+        if self.endpoint is None:
+            raise Exception('Specify endpoint of DirectAdmin')
 
     def _authenticate(self):
         None
