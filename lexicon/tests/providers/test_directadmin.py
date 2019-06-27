08f4e3637eb2bb1719bb6785038f71442f0406cd
Joris Kraak <me@joriskraak.nl>
2019-06-28 00:43:50 +0200

    Strop trying to scrub responses from within the test suite
    This breaks the vcrpy recordings. It's easier to process the recordings
    after the fact removing any sensitive data.

--- lexicon/tests/providers/test_directadmin.py
+++ lexicon/tests/providers/test_directadmin.py
@@ -23,16 +23,6 @@ class DirectAdminProviderTests(TestCase, IntegrationTests):
     def _filter_response(self, response):
         """See `IntegrationTests._filter_response` for more information on how
            to filter the provider response."""
-
-        # The server against which the live tests run has more domains than
-        # relevant for the integration tests, these should not be captured in
-        # recordings
-        if response['body']['string'][0] != '{':
-            # The only non-JSON capable response is the domain list overview,
-            # which should be cleaned of unrelated domain information
-            response['body']['string'] = 'list[]={0}&list[]=anotherdomain.com'.format(self.domain)
-            response['headers']['content-length'] = ['{0}'.format(len(response['body']['string']))]
-
         return response
 
     def _test_parameters_overrides(self):
