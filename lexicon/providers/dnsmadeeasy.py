83a819c84e6dca5c57317bfb73f9716604cc09cd
Adrien Ferrand <ferrand.ad@gmail.com>
2019-10-07 16:44:58 +0200

    Update code

--- lexicon/providers/dnsmadeeasy.py
+++ lexicon/providers/dnsmadeeasy.py
@@ -20,9 +20,10 @@ NAMESERVER_DOMAINS = ['dnsmadeeasy']
 
 class _RetryRateLimit(Retry):
     # Standard urllib3 Retry objects trigger retries only based on HTTP status code or HTTP method.
-    # However we need to differentiate the 400 errors with body `{"error": ["Rate limit exceeded"]}`
+    # However we need to differentiate 400 errors with body `{"error": ["Rate limit exceeded"]}`
     # from the other 400 errors. The internal _RetryRateLimit class does that.
-    def increment(self, method=None, url=None, response=None, error=None, _pool=None, _stacktrace=None):
+    def increment(self, method=None, url=None, response=None,
+                  error=None, _pool=None, _stacktrace=None):
         if response:
             body = json.loads(response.data)
             if 'Rate limit exceeded' in body.get('error', []):
@@ -182,11 +183,13 @@ class Provider(BaseProvider):
 
         session = requests.Session()
         try:
-            # DNSMadeEasy allows only 150 requests in a floating 5 min time window. So we implement a retry
-            # strategy on requests returned as 400 with body `{"error": ["Rate limit exceeded"]}`.
-            # 10 retries with backoff = 0.6 gives following retry delays (in seconds) after first attempt:
-            # 1.2, 2.4, 4.8, 9.6, 19.2, 38.4, 76.8, 153.6, 307.2
-            # So last attempt is done 5 min 7 seconds after first try, so the size of the floating window.
+            # DNSMadeEasy allows only 150 requests in a floating 5 min time window.
+            # So we implement a retry strategy on requests returned as 400 with body
+            # `{"error": ["Rate limit exceeded"]}`.
+            # 10 retries with backoff = 0.6 gives following retry delays after first attempt:
+            # 1.2s, 2.4s, 4.8s, 9.6s, 19.2s, 38.4s, 76.8s, 153.6s, 307.2s
+            # So last attempt is done 5 min 7 seconds after first try, so the
+            # size of the floating window.
             # Beyond it we can assume something else is wrong and so give up.
             session_retries = _RetryRateLimit(total=10, backoff_factor=0.6, status_forcelist=[400])
             session_adapter = HTTPAdapter(max_retries=session_retries)
@@ -196,7 +199,6 @@ class Provider(BaseProvider):
                                        data=json.dumps(data),
                                        headers=default_headers,
                                        auth=default_auth)
-            print(response.headers)
             # if the request fails for any reason, throw an error.
             response.raise_for_status()
 
