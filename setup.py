a9d7d3fd3a1b526a64bfc93c1f9d60b196436d8e
Adrien Ferrand <ferrand.ad@gmail.com>
2019-10-23 15:22:17 +0200

    Add support for Python 3.8

--- setup.py
+++ setup.py
@@ -100,6 +100,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     keywords='dns lexicon dns-lexicon dehydrated letsencrypt ' + ' '.join(providers),
