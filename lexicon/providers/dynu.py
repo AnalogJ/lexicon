f36f28cdc4d9ffa90be176a99e353447de89ad00
Chris Bräucker <7303810+chrisbraucker@users.noreply.github.com>
2019-11-11 21:15:54 +0100

    satisfy test that fails when domain not owned

--- lexicon/providers/dynu.py
+++ lexicon/providers/dynu.py
@@ -32,6 +32,8 @@ class Provider(BaseProvider):
             if domain['name'].lower() == self.domain.lower():
                 self.domain_id = domain['id']
                 break
+        if not self.domain_id:
+            raise Exception('No matching domain found')
 
     # Create record. If record already exists with the same content, do nothing.
     def _create_record(self, rtype, name, content):
