5def37284a9641d63099acf2e5f456b0388223fa
Chris Bräucker <7303810+chrisbraucker@users.noreply.github.com>
2020-03-11 19:07:24 +0100

    Add record mapping to/from dynu
    The Dynu API has very specific demands on how records
    should look like. Where possible, mapping is done in
    both directions to add support for records other than TXT

--- lexicon/providers/dynu.py
+++ lexicon/providers/dynu.py
@@ -37,13 +37,7 @@ class Provider(BaseProvider):
 
     # Create record. If record already exists with the same content, do nothing.
     def _create_record(self, rtype, name, content):
-        record = {
-            'recordType': rtype,
-            'textData': content,
-            'state': True,
-        }
-        if name:
-            record['nodeName'] = self._relative_name(name)
+        record = self._to_dynu_record(rtype, name, content)
 
         if self._get_lexicon_option('ttl'):
             record['ttl'] = self._get_lexicon_option('ttl')
@@ -57,7 +51,7 @@ class Provider(BaseProvider):
                 pass
             else:
                 raise error
-        created = self._format_record(payload)
+        created = self._from_dynu_record(payload)
         LOGGER.debug('create_record: %s', created)
         return created
 
@@ -69,7 +63,7 @@ class Provider(BaseProvider):
 
         records = []
         for record in payload['dnsRecords']:
-            processed_record = self._format_record(record)
+            processed_record = self._from_dynu_record(record)
             records.append(processed_record)
 
         len_all = len(records)
@@ -92,14 +86,10 @@ class Provider(BaseProvider):
 
     # Create or update a record.
     def _update_record(self, identifier, rtype=None, name=None, content=None):
-        record = {
-            'recordType': rtype,
-            'name': name,
-            'content': content,
-        }
+        record = self._to_dynu_record(rtype, name, content)
 
         payload = self._post('/dns/{0}/record/{1}'.format(self.domain_id, identifier), record)
-        update = self._format_record(payload)
+        update = self._from_dynu_record(payload)
         LOGGER.debug('update_record: %s', update)
         return update
 
@@ -137,11 +127,80 @@ class Provider(BaseProvider):
 
     # Takes a Dynu.com record and puts it into lexicon-shape
     @staticmethod
-    def _format_record(record):
+    def _from_dynu_record(record):
+        rtype = record['recordType']
+        options = {
+            'enabled': record['state'],
+            'lastUpdate': record['updatedOn'],
+        }
+
+        # map additional fields depending on the record type
+        # the result takes the record type as key, and all options of the
+        # matching key in the following dict as dict of values,
+        # e.g.
+        # 'options': {
+        #     'CNAME': {
+        #         'host': 'example.com'
+        #     }
+        # }
+        options.update({rtype: {
+            'A': {'ipv4': record['ipv4Address'], 'group': record['group']},
+            'AAAA': {'ipv6': record['ipv6Address'], 'group': record['group']},
+            'CNAME': {'host': record['host']},
+            'LOC': {
+                'lat': record['latitude'],
+                'long': record['longitude'],
+                'alt': {record['altitude']},
+                'size': {record['size']},
+                'hPrec': {record['horizontalPrecision']},
+                'vPrec': {record['verticalPrecision']},
+            },
+            'MX': {'host': record['host'], 'priority': record['priority']},
+            'NS': {'host': record['host']},
+            'PTR': {'host': record['host']},
+            'SPF': {'data': record['textData']},
+            'SRV': {'host': record['host'], 'priority': record['priority'], 'weight': record['weight']},
+            'TXT': {'data': record['textData']},
+        }[rtype]})
+
+        # format the content as noted in the spec, e.g. take everything
+        # in the raw DNS response after the record type, and remove quotations
+        # Example: 
+        #   example.com. 120 IN TXT \"txt-value=thisIsATest\"
+        # Becomes:
+        #   txt-value=thisIsATest
+        content = record['content'].split(rtype)[1].strip().replace('"', '')
         return {
             'id': record['id'],
-            'type': record['recordType'],
+            'type': rtype,
             'name': record['hostname'],
-            'content': record['content'],
             'ttl': record['ttl'],
+            'content': content,
+            'options': options
         }
+
+
+    # Takes record input and puts it into a format the Dynu.com API supports
+    def _to_dynu_record(self, rtype, name, content):
+        if rtype == 'LOC':
+            raise NotImplementedError
+
+        output = {
+            'recordType': rtype,
+            'nodeName': self._relative_name(name),
+            'state': True,
+        }
+
+        cnt_split = content.split(' ')
+        output.update({
+            'A': {'ipv4Address': content},
+            'AAAA': {'ipv6Address': content},
+            'CNAME': {'host': content},
+            'MX': {'priority': cnt_split[0], 'host': cnt_split[1]},
+            'NS': {'host': content},
+            'PTR': {'host': content},
+            'SPF': {'textData': content},
+            'SRV': {'priority': cnt_split[0], 'weight': cnt_split[1], 'port': cnt_split[2], 'host': cnt_split[3]},
+            'TXT': {'textData': content},
+        }[rtype])
+        return output
