27124a0e8f0b17312dba64469df9ece78fd6ccd0
Alex Cortelyou <acortelyou@gmail.com>
2020-05-05 14:59:36 -0700

    Use Bearer Token Authorization for Cloudflare API Tokens (#460)
    * Use Bearer Token Authorization header for API Tokens when Cloudflare username not specified
    
    * With apologies to the linter
    
    * Update and fix tests
    
    * Fix lint
    
    * Fix lint
    
    Co-authored-by: Adrien Ferrand <ferrand.ad@gmail.com>

--- lexicon/providers/cloudflare.py
+++ lexicon/providers/cloudflare.py
@@ -14,10 +14,17 @@ NAMESERVER_DOMAINS = ['cloudflare.com']
 
 def provider_parser(subparser):
     """Return the parser for this provider"""
+    subparser.description = '''
+        There are two ways to provide an authentication granting edition to the target CloudFlare DNS zone.
+        1 - A Global API key, with both --auth-username and --auth-token flags.
+        2 - An API token (permissions Zone:Zone(read) + Zone:DNS(edit) for all zones), with only --auth-token flag.
+    '''
     subparser.add_argument(
-        "--auth-username", help="specify email address for authentication")
+        "--auth-username",
+        help="specify email address for authentication (for Global API key only)")
     subparser.add_argument(
-        "--auth-token", help="specify token for authentication")
+        "--auth-token",
+        help="specify token for authentication (Global API key or API token)")
 
 
 class Provider(BaseProvider):
@@ -103,6 +110,14 @@ class Provider(BaseProvider):
 
     # Create or update a record.
     def _update_record(self, identifier, rtype=None, name=None, content=None):
+        if identifier is None:
+            records = self._list_records(rtype, name)
+            if len(records) == 1:
+                identifier = records[0]['id']
+            elif len(records) < 1:
+                raise Exception('No records found matching type and name - won\'t update')
+            else:
+                raise Exception('Multiple records found matching type and name - won\'t update')
 
         data = {}
         if rtype:
@@ -145,13 +160,15 @@ class Provider(BaseProvider):
             data = {}
         if query_params is None:
             query_params = {}
+        headers = {'Content-Type': 'application/json'}
+        if self._get_provider_option('auth_username'):
+            headers['X-Auth-Email'] = self._get_provider_option('auth_username')
+            headers['X-Auth-Key'] = self._get_provider_option('auth_token')
+        else:
+            headers['Authorization'] = 'Bearer {}'.format(self._get_provider_option('auth_token'))
         response = requests.request(action, self.api_endpoint + url, params=query_params,
                                     data=json.dumps(data),
-                                    headers={
-                                        'X-Auth-Email': self._get_provider_option('auth_username'),
-                                        'X-Auth-Key': self._get_provider_option('auth_token'),
-                                        'Content-Type': 'application/json'
-                                    })
+                                    headers=headers)
         # if the request fails for any reason, throw an error.
         response.raise_for_status()
         return response.json()
