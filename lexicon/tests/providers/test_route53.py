08fdf6534e652ae5c24454fc7f014898d3ee2bec
Adrien Ferrand <ferrand.ad@gmail.com>
2020-05-06 09:40:46 +0200

    Update cassettes for route53, add a dynamic ID

--- lexicon/tests/providers/test_route53.py
+++ lexicon/tests/providers/test_route53.py
@@ -10,12 +10,11 @@ from lexicon.tests.providers.integration_tests import PROVIDER_VCR
 from lexicon.tests.providers.integration_tests import EngineOverrideConfigSource
 
 
-@pytest.mark.skip('Cassettes needs to be regenerated')
 class Route53ProviderTests(TestCase, IntegrationTests):
     """Route53 Proivder Tests."""
 
     provider_name = 'route53'
-    domain = 'capsulecd.com'
+    domain = 'fullcr1stal.tk'
 
     def _filter_headers(self):
         """Sensitive headers to be filtered."""
@@ -47,10 +46,6 @@ class Route53ProviderTests(TestCase, IntegrationTests):
                 filter_query_parameters=self._filter_query_parameters(),
                 filter_post_data_parameters=self._filter_post_data_parameters()):
             yield
-
-    @pytest.mark.skip(reason="route 53 dns records don't have ids")
-    def test_provider_when_calling_delete_record_by_identifier_should_remove_record(self):
-        return
 
     # TODO: the following skipped suite and fixtures should be enabled
     @pytest.mark.skip(reason="new test, missing recording")
