92b6e654b92a4bbc3de60788c3a1588921e8760a
Adrien Ferrand <ferrand.ad@gmail.com>
2020-08-16 17:46:17 +0200

    Fix lint

--- lexicon/tests/providers/test_euserv.py
+++ lexicon/tests/providers/test_euserv.py
@@ -9,15 +9,16 @@ from unittest import TestCase
 
 from lexicon.tests.providers.integration_tests import IntegrationTestsV2
 
-# EUserv has a limit of 10 TXT entries. Therefore, the live recordings were
-# and entries removed manually during the execution of the tests.
-# Otherwise, a test case will fail with a corresponding error (maximum entries reached).
 
 # Hook into testing framework by inheriting unittest.TestCase and reuse
 # the tests which *each and every* implementation of the interface must
 # pass, by inheritance from integration_tests.IntegrationTests
 class EUservProviderTests(TestCase, IntegrationTestsV2):
     """Tests for EUserv"""
+
+    # EUserv has a limit of 10 TXT entries. Therefore, the live recordings were
+    # and entries removed manually during the execution of the tests.
+    # Otherwise, a test case will fail with a corresponding error (maximum entries reached).
 
     provider_name = "euserv"
     domain = "schoettle.it"
