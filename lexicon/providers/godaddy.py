6fcc771c54788ee1078e28c221c55320488abd66
Thomas Francis Jr <franc@tcsaf.com>
2020-08-26 13:23:48 -0400

    Fixed create and update operations for GoDaddy if CAA records are present
    The GoDaddy API breaks (returning 422) if you try to submit a CAA
    record.  So when you create a new record, if the domain has one or more
    CAA records, the create will fail (update and delete fail the same way).
    This change simply submits the new (or updated) record instead of
    submitting all records.
    
    This doesn't address the problems with delete, as the GoDaddy API still
    doesn't let you delete a single record.  So for domains with CAA
    records, any delete action will continue to fail.

--- lexicon/providers/godaddy.py
+++ lexicon/providers/godaddy.py
@@ -94,7 +94,7 @@ class Provider(BaseProvider):
         ttl = self._get_lexicon_option("ttl")
 
         # Retrieve existing data in DNS zone.
-        records = self._get("/domains/{0}/records".format(domain))
+        records = self._get("/domains/{0}/records/{1}/{2}".format(domain, rtype, relative_name))
 
         # Check if a record already matches given parameters
         for record in records:
@@ -115,8 +115,8 @@ class Provider(BaseProvider):
 
         records.append(data)
 
-        # Synchronize data with inserted record into DNS zone.
-        self._put("/domains/{0}/records".format(domain), records)
+        # Insert the record
+        self._put("/domains/{0}/records/{1}/{2}".format(domain, rtype, relative_name), records)
 
         LOGGER.debug("create_record: %s %s %s", rtype, name, content)
 
@@ -142,6 +142,7 @@ class Provider(BaseProvider):
         if name:
             relative_name = self._relative_name(name)
 
+        updated_record = None
         # Retrieve existing data in DNS zone.
         records = self._get("/domains/{0}/records".format(domain))
 
@@ -157,10 +158,16 @@ class Provider(BaseProvider):
                 and record["data"] != content
             ):
                 record["data"] = content
+                updated_record = record
                 break
 
         # Synchronize data with updated records into DNS zone.
-        self._put("/domains/{0}/records".format(domain), records)
+        if updated_record is not None:
+            if identifier and self._relative_name(updated_record['name']) != relative_name:
+                self._put('/domains/{0}/records/{1}'.format(domain, rtype), records)
+            else:
+                self._put('/domains/{0}/records/{1}/{2}'.format(domain, rtype, relative_name),
+                          updated_record)
 
         LOGGER.debug("update_record: %s %s %s", rtype, name, content)
 
