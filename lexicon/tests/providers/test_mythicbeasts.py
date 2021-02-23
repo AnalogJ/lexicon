d92ba6f50f7cbc6cfa8f643f543c74b49641116e
Edward Perkins <ed.perkins@gmail.com>
2021-02-23 19:40:16 +0000

    Fixed stripping token from rest results

--- lexicon/tests/providers/test_mythicbeasts.py
+++ lexicon/tests/providers/test_mythicbeasts.py
@@ -2,7 +2,7 @@
 from unittest import TestCase
 
 from lexicon.tests.providers import integration_tests
-
+import json
 
 # Hook into testing framework by inheriting unittest.TestCase and reuse
 # the tests which *each and every* implementation of the interface must
@@ -22,19 +22,14 @@ class MythicBeastsProviderTests(TestCase, integration_tests.IntegrationTestsV2):
     # def _filter_query_parameters(self):
     #     return ['secret_key']
 
-    # def _filter_response(self, response):
-    #     """See `IntegrationTests._filter_response` for more information on how
-    #     to filter the provider response."""
-    #     return response
+    def _filter_response(self, response):
+        """See `IntegrationTests._filter_response` for more information on how
+        to filter the provider response."""
+        string = response['body']['string']
+        string=json.loads(string)
+        string['access_token']=None
+        response['body']['string']=string
+        return response
 
     def _test_fallback_fn(self):
         return lambda x: "placeholder_" + x if x not in ("auth_token") else ""
-
-    # We override this test because Mythic Beasts refuse to create a A record with 'localhost' value.
-    #  - no - this is ok - problem with pre-existing records
-    # @integration_tests._vcr_integration_test
-    # def test_provider_when_calling_create_record_for_A_with_valid_name_and_content(
-    #     self,
-    # ):
-    #     provider = self._construct_authenticated_provider()
-    #     assert provider.create_record("A", "localhost2", "127.0.0.1")
