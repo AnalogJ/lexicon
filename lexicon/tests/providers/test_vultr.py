ed75f0a87de5131a265a042bf0374f462cb76499
Bartel Sielski <bartel.sielski@gmail.com>
2021-03-28 14:19:48 +0200

    Implement vultr API v2 (#770)
    * Implement vultr API v2
    
    * Update test recordings for vultr API v2

--- lexicon/tests/providers/test_vultr.py
+++ lexicon/tests/providers/test_vultr.py
@@ -1,27 +1,17 @@
 """Integration tests for Vultr"""
 from unittest import TestCase
 
-import pytest
-
-from lexicon.tests.providers.integration_tests import IntegrationTestsV1
+from lexicon.tests.providers.integration_tests import IntegrationTestsV2
 
 
 # Hook into testing framework by inheriting unittest.TestCase and reuse
 # the tests which *each and every* implementation of the interface must
 # pass, by inheritance from define_tests.TheTests
-# TODO: migrate to IntegrationTestsV2 and its extended test suite
-class VultrProviderTests(TestCase, IntegrationTestsV1):
+class VultrProviderTests(TestCase, IntegrationTestsV2):
     """TestCase for Vultr"""
 
     provider_name = "vultr"
-    domain = "capsulecd.com"
+    domain = "lexicon-test.eu"
 
     def _filter_headers(self):
-        return ["API-Key"]
-
-    # TODO: enable the skipped tests
-    @pytest.mark.skip(reason="new test, missing recording")
-    def test_provider_when_calling_update_record_should_modify_record_name_specified(
-        self,
-    ):
-        return
+        return ["Authorization"]
