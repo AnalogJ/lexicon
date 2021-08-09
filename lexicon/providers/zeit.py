cfdbc21269afa2a7bf178743b4cfe6b865f49f6c
Adrien Ferrand <ferrand.ad@gmail.com>
2021-08-09 22:19:34 +0200

    Add compatibility layer

---
+++ lexicon/providers/zeit.py
@@ -0,0 +1,4 @@
+"""Compatibility layer for Zeit (old name for Vercel)"""
+from lexicon.providers.vercel import NAMESERVER_DOMAINS
+from lexicon.providers.vercel import provider_parser
+from lexicon.providers.vercel import Provider
