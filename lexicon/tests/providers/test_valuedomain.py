7eb80c397d3f94263a27f061b7f2798264523eaa
wak109 <wak109@gmail.com>
2021-11-15 18:36:09 +0900

    Value-Domain added

---
+++ lexicon/tests/providers/test_valuedomain.py
@@ -0,0 +1,34 @@
+"""Test for route53 implementation of the interface."""
+from contextlib import contextmanager
+from unittest import TestCase
+
+import pytest
+# Test for one implementation of the interface
+from lexicon.tests.providers.integration_tests import IntegrationTestsV2
+from unittest import TestCase
+
+# Hook into testing framework by inheriting unittest.TestCase and reuse
+# the tests which *each and every* implementation of the interface must
+# pass, by inheritance from integration_tests.IntegrationTests
+class ValueDomainProviderTests(TestCase, IntegrationTestsV2):
+    """Integration tests for Foo provider"""
+    provider_name = 'valuedomain'
+    domain = 'canadian-wisteria.net'
+
+    def _filter_post_data_parameters(self):
+        return ['login_token']
+
+    def _filter_headers(self):
+        return ['Authorization']
+
+    def _filter_query_parameters(self):
+        return ['secret_key']
+
+    def _filter_response(self, response):
+        """See `IntegrationTests._filter_response` for more information on how
+        to filter the provider response."""
+        return response
+
+    @pytest.mark.skip(reason="can not set ttl when creating/updating records")
+    def test_provider_when_calling_list_records_after_setting_ttl(self):
+        return
