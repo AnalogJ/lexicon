fe64b4b0fc382563ebe577009e0b0863fb1d4177
wak109 <wak109@gmail.com>
2021-11-16 13:46:09 +0900

    Format test_valuedomain.py by flake8

--- lexicon/tests/providers/test_valuedomain.py
+++ lexicon/tests/providers/test_valuedomain.py
@@ -1,15 +1,14 @@
-"""Test for route53 implementation of the interface."""
-from contextlib import contextmanager
+"""Test for valuedomain implementation of the interface."""
 from unittest import TestCase
 
-import pytest
 # Test for one implementation of the interface
 from lexicon.tests.providers.integration_tests import IntegrationTestsV2
-from unittest import TestCase
 
 # Hook into testing framework by inheriting unittest.TestCase and reuse
 # the tests which *each and every* implementation of the interface must
 # pass, by inheritance from integration_tests.IntegrationTests
+
+
 class ValueDomainProviderTests(TestCase, IntegrationTestsV2):
     """Integration tests for Foo provider"""
     provider_name = 'valuedomain'
