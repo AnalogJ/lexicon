e563f1b684ef0019041525a07a068e05e4511c8b
Adrien Ferrand <ferrand.ad@gmail.com>
2021-11-24 09:33:35 +0100

    Take description of the provider into account in the doc

--- utils/generate_provider_doc.py
+++ utils/generate_provider_doc.py
@@ -2,6 +2,7 @@
 import argparse
 import importlib
 import os
+import re
 
 from lexicon import discovery
 
@@ -36,6 +37,15 @@ List of options
 {provider}
 """
         ]
+
+        if parser.description:
+            provider_content.append(f"""
+.. note::
+   
+{_cleanup_description(parser.description)}
+
+""")
+
         for action in parser._actions:
             if action.dest == "help":
                 continue
@@ -68,6 +78,19 @@ def _generate_table(items):
         table = [*table, line, delimiter]
 
     return "\n".join(table)
+
+def _cleanup_description(description: str):
+    lines = description.split(os.linesep)
+    if not lines:
+        return ""
+    if not lines[0]:
+        lines.pop(0)
+    if not lines:
+        return ""
+    match = re.match(r"^(\s*)\S.*$", lines[0])
+    first_ident = len(match.group(1)) if match else 0
+    lines = [f"   {line[first_ident:]}" for line in lines]
+    return os.linesep.join(lines)
 
 
 if __name__ == "__main__":
