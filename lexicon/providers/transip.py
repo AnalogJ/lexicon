7ea2673e1d6615e877d0b97c145ac7cc31a90f65
Adrien Ferrand <ferrand.ad@gmail.com>
2022-01-26 23:35:22 +0100

    Use appropriate JSONDecodeError for retrocompatibility purposes

--- lexicon/providers/transip.py
+++ lexicon/providers/transip.py
@@ -11,7 +11,11 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
-from requests.exceptions import JSONDecodeError
+
+try:
+    from simplejson import JSONDecodeError
+except ImportError:
+    from json import JSONDecodeError
 
 from lexicon.exceptions import LexiconError
 from lexicon.providers.base import Provider as BaseProvider
