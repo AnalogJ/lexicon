40b505befb8c2de2e6f156669e57941f9a98c69b
Adrien Ferrand <ferrand.ad@gmail.com>
2022-04-19 22:58:58 +0200

    Update route53 tests

--- lexicon/tests/providers/test_route53.py
+++ lexicon/tests/providers/test_route53.py
@@ -16,18 +16,21 @@ class Route53ProviderTests(TestCase, integration_tests.IntegrationTestsV2):
     def _filter_headers(self):
         """Sensitive headers to be filtered."""
         return ["Authorization"]
+    
+    def _test_fallback_fn(self):
+        return lambda x: None if x in ("zone_id") else f"placeholder_{x}"
 
+    @integration_tests.vcr_integration_test
     def test_provider_authenticate_private_zone_only(self):
-        with self._use_vcr("IntegrationTests/test_provider_authenticate.yaml"):
-            provider = self._build_provider_with_overrides({"private_zone": "true"})
-            with pytest.raises(Exception):
-                provider.authenticate()
-
-    def test_provider_authenticate_private_zone_false(self):
-        with self._use_vcr("IntegrationTests/test_provider_authenticate.yaml"):
-            provider = self._build_provider_with_overrides({"private_zone": "false"})
+        provider = self._build_provider_with_overrides({"private_zone": "true"})
+        with pytest.raises(Exception):
             provider.authenticate()
-            assert provider.domain_id is not None
+
+    @integration_tests.vcr_integration_test
+    def test_provider_authenticate_private_zone_false(self):
+        provider = self._build_provider_with_overrides({"private_zone": "false"})
+        provider.authenticate()
+        assert provider.domain_id is not None
 
     def _build_provider_with_overrides(self, overrides):
         config = self._test_config()
@@ -35,14 +38,3 @@ class Route53ProviderTests(TestCase, integration_tests.IntegrationTestsV2):
             integration_tests.EngineOverrideConfigSource(overrides), 0
         )
         return self.provider_module.Provider(config)
-
-    @contextmanager
-    def _use_vcr(self, path):
-        with integration_tests.PROVIDER_VCR.use_cassette(
-            self._cassette_path(path),
-            filter_headers=self._filter_headers(),
-            before_record_response=self._filter_response,
-            filter_query_parameters=self._filter_query_parameters(),
-            filter_post_data_parameters=self._filter_post_data_parameters(),
-        ):
-            yield
