498e940e5b4b5a7a3f9428590913bbea2b39459d
Adrien Ferrand <ferrand.ad@gmail.com>
2022-08-11 23:42:27 +0200

    This is fun

--- lexicon/parser.py
+++ lexicon/parser.py
@@ -8,7 +8,7 @@ from lexicon import discovery
 
 def generate_base_provider_parser() -> argparse.ArgumentParser:
     """Function that generates the base provider to be used by all dns providers."""
-    parser = argparse.ArgumentParser(add_help=False)
+    parser = argparse.ArgumentParser(add_help=False, prog="base")
     parser.add_argument(
         "action",
         help="specify the action to take",
@@ -54,7 +54,7 @@ def generate_base_provider_parser() -> argparse.ArgumentParser:
 def generate_cli_main_parser() -> argparse.ArgumentParser:
     """Using all providers available, generate a parser that will be used by Lexicon CLI"""
     parser = argparse.ArgumentParser(
-        description="Create, Update, Delete, List DNS entries"
+        description="Create, Update, Delete, List DNS entries", prog="lexicon"
     )
 
     parser.add_argument(
